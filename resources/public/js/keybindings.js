d36bccd3c84f99aa9de1917e19918c0baed048ab
Roey Chasman <rchasman@email.arizona.edu>
2013-02-27 11:10:34 -0700

    Fixed keybindings to update the graph shown.

--- resources/public/js/keybindings.js
+++ resources/public/js/keybindings.js
@@ -1,4 +1,13 @@
-//Pan and Select keybindings
+// Handle Mousewheel Delta
+function handle(delta) {
+    if (delta.wheelDelta < 0) {
+        zoomOut();
+    } else {
+        zoomIn();
+    }
+}
+
+// Pan and Select Keybindings
 Mousetrap.bind(['p'], function() {
     $('#rb2').prop('checked', true);
     $('#rb1').prop('checked', false);
@@ -11,14 +20,14 @@ Mousetrap.bind(['s'], function() {
     setPanSelect();
 });
 
-//Search on Enter keybinding
+// Search on Enter Keybinding
 $(document).keydown(function(e){
     if (e.keyCode == 13) {
         searchChart();
     }
 });
 
-//Arrow Key navigation control keybindings
+// Arrow Key Navigation Keybindings
 Mousetrap.bind(['up'], function() {
     event.preventDefault();
     zoomIn();
@@ -30,17 +39,42 @@ Mousetrap.bind(['down'], function() {
 });
 
 Mousetrap.bind(['left'], function() {
-    var graph = chart.panels[0].graphs[0];
+    var graph = chart.panels[0];
     event.preventDefault();
     if (graph.start > 0) {
         chart.panels[0].zoomToIndexes(graph.start - 5, graph.end - 5);
     }
+    chart.scrollbarChart.zoom(chart.panels[0].start, chart.panels[0].end)
 });
 
 Mousetrap.bind(['right'], function() {
-    var graph = chart.panels[0].graphs[0];
+    var graph = chart.panels[0];
     event.preventDefault();
-    if (graph.end + 2 < graph.data.length) {
+    if (graph.end + 2 < graph.chartData.length) {
         chart.panels[0].zoomToIndexes(graph.start + 5, graph.end + 5);
     }
+    chart.scrollbarChart.zoom(chart.panels[0].start, chart.panels[0].end)
 });
+
+// Zoom Functions
+function zoomIn() {
+    var graph = chart.panels[0];
+    var end = graph.chartData.length;
+    if (graph.end - 2 > graph.start && graph.end < end) {
+        chart.panels[0].zoomToIndexes(graph.start + 1, graph.end - 1);
+    }
+    chart.scrollbarChart.zoom(chart.panels[0].start, chart.panels[0].end)
+}
+
+function zoomOut() {
+    var graph = chart.panels[0];
+    var end = graph.chartData.length;
+    if (graph.start > 0 && graph.end < end) {
+        chart.panels[0].zoomToIndexes(graph.start - 1, graph.end + 1);
+    } else if (graph.end < end){
+        chart.panels[0].zoomToIndexes(graph.start, graph.end + 1);
+    } else {
+        chart.panels[0].zoomToIndexes(graph.start -1, graph.end);
+    }
+    chart.scrollbarChart.zoom(chart.panels[0].start, chart.panels[0].end)
+}
